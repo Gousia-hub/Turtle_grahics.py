d3e6244c525bd3019128bd6bad53b9fbd020b46d
Gousia-hub <84321414+Gousia-hub@users.noreply.github.com>
2021-12-10 22:32:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,3 @@ my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
 
-# from prettytable import PrettyTable
-# table = PrettyTable()
-# table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Butterfree"])
-# table.add_column("Type", ["Electric", "Aqua", "Flyer"])
-# table.align = "l"
-# table.valign = "b"
-# print(table)
